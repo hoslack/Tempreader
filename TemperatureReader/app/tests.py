0c595f8a953e31a14e78c49a411a9b9a0106991f
HOSLACK <hoslackochieng@gmail.com>
2017-05-02 09:45:09 +0300

    Add viewtests

--- TemperatureReader/app/tests.py
+++ TemperatureReader/app/tests.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+
+class AppViewsTestCase(TestCase):
+    def test_index(self):
+        resp = self.client.get('/app/')
+        self.assertEqual(resp.status_code, 200)
+
+
