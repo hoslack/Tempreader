6c0eba36ff27e38f3a22d8e67c71efe4eb672c49
HOSLACK <hoslackochieng@gmail.com>
2017-05-02 10:38:02 +0300

    Travis test

--- TemperatureReader/app/tests.py
+++ TemperatureReader/app/tests.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
+from unittest import *
+from views import *
+from requests import get, post
 
 
 class AppViewsTestCase(TestCase):
     def test_index(self):
         resp = self.client.get('/app/')
         self.assertEqual(resp.status_code, 200)
+
+    def test_series_returns_path(self):
+        self.assertEqual(timeseries(get), 'file:///home/hos/Desktop/Tempreader/TemperatureReader/temp-plot.html')
+
+
+
+
 
 
