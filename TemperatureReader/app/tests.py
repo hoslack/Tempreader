9fccc93214c3bcf0ba45e90e85e176a16a06e06c
HOSLACK <hoslackochieng@gmail.com>
2017-05-03 01:52:41 +0300

    Adding more tests and finalizing the project

--- TemperatureReader/app/tests.py
+++ TemperatureReader/app/tests.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.test import TestCase
 from unittest import *
 from views import *
 from requests import get, post
+from models import Data
+import datetime
 
 
 class AppViewsTestCase(TestCase):
@@ -14,6 +15,13 @@ class AppViewsTestCase(TestCase):
     def test_series_returns_path(self):
         self.assertEqual(timeseries(get), 'file:///home/hos/Desktop/Tempreader/TemperatureReader/temp-plot.html')
 
+    def test_data_instance(self):
+        reading = Data()
+        self.assertIsInstance(reading, Data, msg='The object should be an instance of the `Data` class')
+
+    def test_datetime_type(self):
+        reading = Data()
+        self.assertIsInstance(reading.time_read, datetime, msg='The datetime data should be of type datetime')
 
 
 
